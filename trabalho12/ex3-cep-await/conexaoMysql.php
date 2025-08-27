370732279d9acc9044fe474071e7b527261e3361
Enzo Lazzarini <enzolazzamor@gmail.com>
2025-08-27 19:14:05 -0300

    chore: fixed sql con and added index2

--- trabalho12/ex3-cep-await/conexaoMysql.php
+++ trabalho12/ex3-cep-await/conexaoMysql.php
@@ -1,23 +1,23 @@
+
 <?php
 
 function mysqlConnect()
 {
-  $db_host = "localhost";
-  $db_username = "ppi";
-  $db_password = "ppi2023";
-  $db_name = "ppi";
+    $db_host = 'sql208.infinityfree.com';
+    $db_username = 'if0_39210028';
+    $db_password = 'HK08op3RNR';
+    $db_name = 'if0_39210028_ppi';
 
-  $options = [
-    PDO::ATTR_EMULATE_PREPARES => false, // desativa a execução emulada de prepared statements
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-  ];
+    $options = [
+        PDO::ATTR_EMULATE_PREPARES => false, // desativa a execução emulada de prepared statements
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ];
 
-  try {
-    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_username, $db_password, $options);
-    return $pdo;
-  } 
-  catch (Exception $e) {
-    exit('Ocorreu uma falha na conexão com o MySQL: ' . $e->getMessage());
-  }
+    try {
+        $pdo = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8mb4", $db_username, $db_password, $options);
+
+        return $pdo;
+    } catch (Exception $e) {
+        exit('Ocorreu uma falha na conexão com o MySQL: ' . $e->getMessage());
+    }
 }
-?>
